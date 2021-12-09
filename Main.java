1826fbe48f4d57051d09ce4444c59a59fbf7ebe1
Isaac Patterson <85522516+IsaacPatterson5@users.noreply.github.com>
2021-12-09 03:44:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;    // Needed for Scanner class
+
+/**
+ *  This program demonstrates keyboard input.
+ */
+public class Main
+{
+   public static void main(String[] args)
+   {
+      int length;    // To hold rectangle's length.
+      int width;     // To hold rectangle's width.
+      int area;      // To hold rectangle's area
+
+      // Create a Scanner object to read input.
+      Scanner console = new Scanner(System.in);
+
+      // Get length from the user.
+      System.out.print("Enter length ");
+      length = console.nextInt();
+
+      // Get width from the user.
+      System.out.print("Enter width ");
+      width = console.nextInt();
+
+      // Calculate area.
+      area = length * width;
+
+      // Display area.
+      System.out.println("The area of rectangle is " + area);
+      System.out.println("");
+
+      String name; // To hold the employee's name
+        int age; // To hold the employee's age
+        char gender; // To hold the employee's gender
+        double salary; // To hold the employee's salary
+
+        // Create a Scanner object to read input.
+        Scanner scanner = new Scanner(System.in);
+
+        // Get the employee's name
+        System.out.print("Enter name: ");
+        name = console.nextLine();
+
+        // Get the employee's age
+        System.out.print("Enter age: ");
+        age = console.nextInt();
+
+        // Get the employee's gender
+        System.out.print("Enter gender: ");
+        gender = console.next().charAt(0);
+
+        // Get the employee's salary
+        System.out.print("Enter salary: ");
+        salary = console.nextDouble();
+
+        // Display the information
+        System.out.println("Name: " + name + " Age: " + age + " Gender: "
+                + gender + " Salary: " + salary);
+   }
+}
